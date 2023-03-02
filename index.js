7db5c242cf14f24d7749054544b8800722368110
Kieren Hetherington <98293613+KHetherington89@users.noreply.github.com>
2023-03-02 08:57:53 +0000

    Create basic layout divs in HTML.

---
+++ index.js
@@ -0,0 +1,6 @@
+
+
+
+
+
+footer.innerHTML = `<a style="color:black; text-decoration:none" href="https://github.com/KHetherington89/signup_form">Copyright © ${new Date().getFullYear()} KHetherington89</a>`;
