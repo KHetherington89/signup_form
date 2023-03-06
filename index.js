94d506c30360a72688994e6fd3c2a0ec1edad582
Kieren Hetherington <98293613+KHetherington89@users.noreply.github.com>
2023-03-06 20:20:59 +0000

    Add hero images and some text. Refine styling.

--- index.js
+++ index.js
@@ -3,4 +3,5 @@
 
 
 
-footer.innerHTML = `<a style="color:black; text-decoration:none" href="https://github.com/KHetherington89/signup_form">Copyright © ${new Date().getFullYear()} KHetherington89</a>`;
+footer.innerHTML = `<a style="color:black; text-decoration:none" href="https://github.com/KHetherington89/signup_form">Copyright © ${new Date().getFullYear()} KHetherington89 
+<i class="fa fa-github" style="font-size:24p; color:black"></i></a>`;
---
+++ readme.md
@@ -0,0 +1,3 @@
+https://www.pexels.com/photo/rocky-ocean-coast-against-starry-sky-4254552/
+https://www.pngwing.com/en/free-png-nthzh
+https://www.pngmart.com/files/17/Shoe-Footprints-PNG-Transparent-Image.png
