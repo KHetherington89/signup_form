5d3a3d60ca0dda2e3383e9736ff4ea46cdcc741e
Kieren Hetherington <98293613+KHetherington89@users.noreply.github.com>
2023-03-12 18:58:51 +0000

    Finish styling. Add JS validation on some elememts

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const email = document.getElementById("email");
+const tel = document.getElementById("tel");
+const password = document.getElementById("password");
+const passwordConfirm = document.getElementById("passwordConfirm");
+const sideMessageText = document.getElementById("sideMessageText");
+const submit = document.getElementById("submit");
+const required = document.getElementById("form").querySelectorAll("[required]")
+const reqArray = Object.values(required);
 
 
+passwordConfirm.addEventListener("input", ()=> passCheck());
+password.addEventListener("input", ()=> passCheck());
 
+function passCheck(){
+    if(passwordConfirm.value != password.value){
+        sideMessageText.innerText = "Your passwords do not match!"
+        passwordConfirm.setCustomValidity("Your passwords do not match!")
+    } 
+    else{
+        sideMessage.textContent = "";
+        passwordConfirm.setCustomValidity("")
+    }
+}
 
+reqArray.forEach(input => {
+    input.addEventListener("input", () => validRelease());
+})
 
-footer.innerHTML = `<a style="color:black; text-decoration:none" href="https://github.com/KHetherington89/signup_form">Copyright © ${new Date().getFullYear()} KHetherington89 
-<i class="fa fa-github" style="font-size:24p; color:black"></i></a>`;
+function validRelease(){
+    let allValid = reqArray.every((input) => {
+        if(input.validity.valid){
+            return true;
+        }
+    })
+    console.log(allValid);
+    if(allValid == true){
+        submit.disabled = false;
+    }
+    else{
+        submit.disabled = true;
+    }
+}
+
+footer.innerHTML = `<a style="color:black; text-decoration:none" href="https://github.com/KHetherington89/signup_form">Copyright © ${new Date().getFullYear()} KHetherington89
+<i class="fa fa-github" style="font-size:24p; color:black"></i></a>`
--- readme.md
+++ readme.md
@@ -1,3 +1,16 @@
+This page was created as part of the Odin Project intermediate HTML/CSS course.
+
+The page is primarily layed out using flexbox, with grid layout for the form
+elements.
+
+Most of the form validation is done with the built-in validation, such as
+patterns and length requirements. Custom JS validation is done to check that 
+passwors are the same, and that all require fields are filled.
+
+CSS effects prompt users as to the validity of their inputs.
+
+
+Ocean image:
 https://www.pexels.com/photo/rocky-ocean-coast-against-starry-sky-4254552/
-https://www.pngwing.com/en/free-png-nthzh
+Footprints image:
 https://www.pngmart.com/files/17/Shoe-Footprints-PNG-Transparent-Image.png
